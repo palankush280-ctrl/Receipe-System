4699ffd32df459bf38c4173b6c2ec5fdbe1ff2f8
palankush280-ctrl <palankush280@gmail.com>
2026-07-03 20:39:02 +0530

    secure SECRET_KEY using .env

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=_^!irh7@=&lml)=hg+a4ta037m7@s+06=u7*_!t^7se9#%0u_'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
